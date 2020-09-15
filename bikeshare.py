3a206bc0a426e3d057a98eb863c5b8792308c0bf
samed.goksoy <msametgoksoy@gmail.com>
2020-09-15 22:56:40 +0300

    definition added to time_stats function

--- bikeshare.py
+++ bikeshare.py
@@ -86,7 +86,7 @@ def load_data(city, month, day):
 
     return df
 
-
+#time_stats function find the most common month, day, and hour values
 def time_stats(df):
     """Displays statistics on the most frequent times of travel."""
 
